01f3e0fcfc19c0eb4d2cd76213963b0e076e8e46
Dominika <domi_nika@hotmail.co.uk>
2021-03-20 11:31:49 +0000

    built file structure and began adding boiler plate

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+let timeLeft = 0;
+let points = 0;
+
+const question1 = {
+  questionText = "This is the question";
+  answerChoices = ["",""];
+  correctAnswer =  answerChoices[3];
+}
+
+document.getElementById("startButton").addEventListener("click", startQuiz);
+
+function startQuiz () {
+  console.log("Hey")
+  // change layout of html to include questions
+
+  // start timer
+}
+
+function q1ValuePopulator () {
+  // populate new layout with question one info
+
+}
+
+function q1AnswerValidator () {
+  //if button text matches correct answer go add points and go to next question
+
+  //if wrong take away time and stay on page
+
+}
--- script.js
+++
@@ -1,8 +0,0 @@
-let timeLeft = 0;
-let points = 0;
-
-document.getElementById("startButton").addEventListener("click", startQuiz);
-
-function startQuiz () {
-  console.log("Hey")
-}
