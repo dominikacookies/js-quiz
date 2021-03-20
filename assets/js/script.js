2c9ec378a01ba231456fcf5e7b3b3a69f9cc597b
Dominika <domi_nika@hotmail.co.uk>
2021-03-20 22:36:30 +0000

    questions screen populates with q1 values

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ const question1 = {
 
 console.log  (question1.correctAnswer())
 
-const constructAnswerButtons = () => {
+const constructAnswerButtons = (quizContentDiv) => {
   const answerButton = document.createElement("button");
   console.log (answerButton)
   return answerButton;
@@ -23,19 +23,36 @@ const constructQuizContentDiv = () => {
   // construct div
   const quizContentDiv = document.createElement("div");
   // add a class
-  quizContentDiv.setAttribute("class", "quizContentt");
+  quizContentDiv.setAttribute("class", "quizContent");
   // append it under the quiz content div
   document.getElementById("container").appendChild(quizContentDiv);
   // add h1
   const quizContentH1 = document.createElement("h1");
   //append under content div
   quizContentDiv.appendChild(quizContentH1);
-  quizContentH1.textContent = question1.questionText ;
+  quizContentH1.textContent = question1.questionText;
   // add buttons - why does this not work?
-  //const answerButtons = question1.answerChoices.every(constructAnswerButtons);
-  
-  //console.log (answerButton);
-  // add ids
+  const button1 = document.createElement("button");
+  button1.setAttribute("id", "#1");
+  button1.textContent = question1.answerChoices[0];
+  quizContentDiv.appendChild(button1);
+
+  const button2 = document.createElement("button");
+  button2.setAttribute("id", "#2");
+  button2.textContent = question1.answerChoices[1];
+  quizContentDiv.appendChild(button2);
+
+  const button3 = document.createElement("button");
+  button3.setAttribute("id", "#3");
+  button3.textContent = question1.answerChoices[2];
+  quizContentDiv.appendChild(button3);
+
+  const button4 = document.createElement("button");
+  button4.setAttribute("id", "#4");
+  button4.textContent = question1.answerChoices[3];
+  quizContentDiv.appendChild(button4);
+
+  console.log (button1);
 
   //append under content div
   //quizContentDiv.appendChild(answerButtons);
